19294acddc6e73537e791583ee023bfe709bf799
shakil <mdshakilhossain720@gmail.com>
2024-10-03 13:17:23 +0600

    upload project

---
+++ lib/screen/views/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../widgets/tab_bar_view/message_tab_all.dart';
+import '../Login_signUp/profile_screen.dart';
+import '../Login_signUp/shedule_time.dart';
+import 'dash_board.dart';
+
+
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<IconData> icons = [
+    FontAwesomeIcons.home,
+    FontAwesomeIcons.envelope,
+    FontAwesomeIcons.clipboardCheck,
+    FontAwesomeIcons.user,
+  ];
+
+  int page = 0;
+
+  List<Widget> pages = [
+    Dashboard(), // You can replace this with your actual pages
+    message_tab_all(),
+    shedule_screen(),
+    Profile_screen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[page], // Display the selected page
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: icons,
+        iconSize: 20,
+        activeIndex: page,
+        height: 80,
+        splashSpeedInMilliseconds: 300,
+        gapLocation: GapLocation.none,
+        activeColor: const Color.fromARGB(255, 0, 190, 165),
+        inactiveColor: const Color.fromARGB(255, 223, 219, 219),
+        onTap: (int tappedIndex) {
+          setState(() {
+            page = tappedIndex; // Update the selected page
+          });
+        },
+      ),
+    );
+  }
+}
